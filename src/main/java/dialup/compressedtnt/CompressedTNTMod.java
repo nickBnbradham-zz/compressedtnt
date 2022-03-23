e7af9fdba4c293e019bac2c26fec571ab84b5697
nickBnbradham <bradhamfixit@gmail.com>
2022-03-23 11:25:07 -0600

    version bump

--- src/main/java/dialup/compressedtnt/CompressedTNTMod.java
+++ src/main/java/dialup/compressedtnt/CompressedTNTMod.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import dialup.compressedtnt.proxies.CommonProxy;
 
-@Mod(modid = CompressedTNTMod.MODID, name = "Compressed TNT", version = "1.0")
+@Mod(modid = CompressedTNTMod.MODID, name = "Compressed TNT", version = "a1.1")
 public class CompressedTNTMod {
 	public static final String MODID = "compressedtnt";
 
